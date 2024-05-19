8413b6d6129b922068fc56ac5362b54d9b974dc5
Seba <johanguevara1221@gmail.com>
2024-05-19 13:53:57 -0300

    Server Deployed

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -13,25 +13,6 @@ export const getUsers = async (req: Request, res: Response) => {
 	}
 }
 
-export const createUser = async (req: Request, res: Response) => {
-	try {
-		const { name, last_name, email, password } = req.body
-
-		const user = await prisma.user.create({
-			data: {
-				name,
-				email,
-				password,
-				last_name,
-			},
-		})
-
-		handleSuccess(res, user)
-	} catch (error) {
-		handleError(res, error)
-	}
-}
-
 export const deleteUser = async (req: Request, res: Response) => {
 	try {
 		const { id } = req.params
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,10 @@
-import { registerValidator } from "../validators/auth";
-import { createUser, deleteUser, getUsers, updateUser } from "../controllers/users.controller";
-import { Router } from "express";
+import { deleteUser, getUsers, updateUser } from '../controllers/users.controller'
+import { Router } from 'express'
 
-const router = Router();
+const router = Router()
 
-router.get("/", getUsers);
-router.post("/", registerValidator, createUser);
-router.delete("/:id", deleteUser);
-router.put("/:id", updateUser);
+router.get('/', getUsers)
+router.delete('/:id', deleteUser)
+router.put('/:id', updateUser)
 
-export default router;
+export default router
