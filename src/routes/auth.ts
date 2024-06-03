53ef1b8e23b98363bab595b9048943296ee383bd
Seba <johanguevara1221@gmail.com>
2024-06-03 11:04:46 -0300

    Fixed Validators Auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { login, checkToken, register } from '../controllers/auth.controller'
 import { tokenAuthMiddleware } from '../middlewares/validateToken'
+import { loginValidator, registerValidator } from '../validators/auth'
 
 const router = Router()
 
-router.post('/login', login)
-router.post('/register', register)
+router.post('/login', loginValidator, login)
+router.post('/register', registerValidator, register)
 router.post('/check-token', tokenAuthMiddleware, checkToken)
 
 export default router
